040eac969980ee42cbd0d758975ea8a43303a2d7
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2017-06-05 12:36:50 -0400

    Step 4 done: fixed up books API

--- books-api/common/models/books.js
+++ books-api/common/models/books.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Books) {
+  Books.prototype.getDownloadPath = function getDownloadPath(done){
+    return done(null, this.filepath);
+  };
 
+  Books.remoteMethod(
+    'prototype.getDownloadPath',
+    {
+      returns: {type: 'string', root: true},
+      http: {path: '/getDownloadPath', verb: 'get'},
+      description: 'Sends download path for book purchases'
+    }
+  );
+
+  return Books;
 };
