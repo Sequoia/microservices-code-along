971f4e23b956eba49de2c5449a27e7b6431f6bab
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2017-06-14 22:10:58 -0400

    Step 14: Encrypting JWT_SECRET done encrypted secrets file added

--- download-service/books.js
+++ download-service/books.js
@@ -3,6 +3,7 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({params: { Bucket: process.env.BOOKS_BUCKET } });
 const jwt = require('jsonwebtoken');
+const kms = new AWS.KMS();
 
 module.exports.download = (event, context, callback) => {
   const token = event.queryStringParameters ? event.queryStringParameters.token : null;
@@ -18,24 +19,23 @@ module.exports.download = (event, context, callback) => {
 
   console.log(`looking up ${token}`);
 
-  let payload;
-  try{
-    payload = jwt.verify(token, process.env.JWT_SECRET);
-  }catch(e){
-    console.error('token problem!');
-    return callback(null, {
-      statusCode: 403,
-      body: JSON.stringify({
-        message: e.message
-      })
-    });
-  }
-
-  console.log(`user (${payload.username}) is downloading (${payload.filename})`);
-  
-  s3.getObject({
-    Key: payload.filename
-  }).promise()
+  decrypt(process.env.JWT_SECRET)
+    .then(function verifyToken(JWT_SECRET){
+      try{
+        let payload = jwt.verify(token, JWT_SECRET);
+        console.log(`user (${payload.username}) is downloading (${payload.filename})`);
+        return payload;
+      }catch(e){
+        console.error('token problem!');
+        e.statusCode = 403;
+        throw e;
+      }
+    })
+    .then(payload => {
+      return s3.getObject({
+        Key: payload.filename
+      }).promise()
+    })
     .then(response => {
       console.log(response);
       return callback(null, {
@@ -59,3 +59,22 @@ module.exports.download = (event, context, callback) => {
 
 };
 
+
+function decrypt(ciphertext){
+  console.log(`decrypting ${ciphertext}`);
+
+  return kms.decrypt({
+    CiphertextBlob: Buffer(ciphertext, 'base64')
+  })
+  .promise()
+  .then(data => String(data.Plaintext))
+  .then(plaintext => {
+    console.log(`decrypted: ${plaintext}`);
+    return plaintext;
+  })
+  .catch(e => {
+    console.error('decryption problem!');
+    console.error(e);
+    throw e;
+  })
+}
